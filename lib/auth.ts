beb3d9f148d4e4dac3ff43bb7c79cfde5189dc5a
NZYM <enzsama.dev@gmail.com>
2025-03-15 16:57:41 +0100

    Generate auth migrations

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,9 +2,13 @@ import { betterAuth, BetterAuthOptions } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db/drizzle";
 import { sendEmail } from "@/actions/email";
+import { account, session, user, verification } from "@/db/schema/auth-schema";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, { provider: "pg" }),
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: { user, session, account, verification },
+  }),
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: true,
