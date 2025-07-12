d2cd730dd8aefb0d233ac2bec79aab4d4af31d18
NZYM <enzsama.dev@gmail.com>
2025-07-12 23:57:40 +0100

    Add note schema and note server actions

---
+++ actions/note-actions.ts
@@ -0,0 +1,93 @@
+"use server";
+import { db } from "@/db/drizzle";
+import { note, noteVersion, noteCollaborator } from "@/db/schema/note-schema";
+import { eq, sql } from "drizzle-orm";
+import { _success } from "zod/v4/core";
+
+// POST
+
+export const createNote = async (ownerId: string, title: string) => {
+  const [newNote] = await db
+    .insert(note)
+    .values({ ownerId, title })
+    .returning();
+  await db
+    .insert(noteCollaborator)
+    .values({ noteId: newNote.id, userId: ownerId, role: "owner" });
+
+  const { content, ...plainDoc } = newNote;
+  return plainDoc;
+};
+
+export const createNewVersion = async (noteId: string) => {
+  const [currentNote] = await db
+    .select()
+    .from(note)
+    .where(eq(note.id, noteId))
+    .limit(1);
+
+  if (currentNote)
+    await db.insert(noteVersion).values({
+      noteId,
+      content: currentNote.content,
+      createdBy: currentNote.ownerId,
+    });
+};
+
+export const addCollaborator = async (noteId: string, userId: string) => {
+  try {
+    await db
+      .insert(noteCollaborator)
+      .values({ noteId, userId, role: "editor" });
+    return { success: true };
+  } catch (error) {
+    return { success: false, error: error };
+  }
+};
+
+// PUT | PATCH
+
+export const updateTitle = async (noteId: string, newTitle: string) => {
+  const [currentNote] = await db
+    .select()
+    .from(note)
+    .where(eq(note.id, noteId))
+    .limit(1);
+
+  if (currentNote)
+    await db.update(note).set({ title: newTitle }).where(eq(note.id, noteId));
+};
+
+export const updateContent = async (noteId: string, newContent: string) => {
+  const [currentNote] = await db
+    .select()
+    .from(note)
+    .where(eq(note.id, noteId))
+    .limit(1);
+
+  if (currentNote)
+    await db
+      .update(note)
+      .set({ content: newContent })
+      .where(eq(note.id, noteId));
+};
+
+// DELETE
+
+export const removeCollaborator = async (noteId: string, userId: string) => {
+  try {
+    await db
+      .delete(noteCollaborator)
+      .where(
+        sql`${noteCollaborator.noteId} = ${noteId} and ${noteCollaborator.userId} = ${userId}`
+      );
+    return { success: true };
+  } catch (error) {
+    return { success: false, error: error };
+  }
+};
+
+export const deleteNote = async (noteId: string) => {
+  await db.delete(noteCollaborator).where(eq(noteCollaborator.noteId, noteId));
+  await db.delete(note).where(eq(note.id, noteId));
+};
---
+++ db/schema/note-schema.ts
@@ -0,0 +1,55 @@
+import {
+  pgTable,
+  primaryKey,
+  text,
+  integer,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import { user } from "./auth-schema";
+import { sql } from "drizzle-orm";
+
+// FOR YDOC DATA IN THE FUTURE
+// const bytea = customType<{ data: Buffer; notNull: true; default: true }>({
+//   dataType() {
+//     return "bytea";
+//   },
+// });
+
+export const note = pgTable("note", {
+  id: text()
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
+  ownerId: text("owner_id")
+    .notNull()
+    .references(() => user.id),
+  title: text("title").notNull().default("Untitled"),
+  content: text("string").notNull().default(""),
+  createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
+});
+
+export const noteVersion = pgTable("note_version", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  noteId: text("note_id")
+    .notNull()
+    .references(() => note.id),
+  content: text("string").notNull(),
+  createdBy: text("created_by")
+    .notNull()
+    .references(() => user.id),
+  createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
+});
+
+export const noteCollaborator = pgTable(
+  "note_collaborator",
+  {
+    noteId: text("note_id")
+      .notNull()
+      .references(() => note.id),
+    userId: text("user_id")
+      .notNull()
+      .references(() => user.id),
+    role: text("role").default("editor").notNull(),
+  },
+  (table) => [primaryKey({ columns: [table.noteId, table.userId] })]
+);
